66b3e0292751f5ebc82112a9a4c30f9186cff7fd
Sergio Tomas <sergio.tomas@feverup.com>
2018-03-02 16:39:16 +0100

    Update settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -57,7 +57,7 @@ X_FRAME_OPTIONS = 'DENY'
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['madtaxiapp.com', ])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['madtaxiapp.com', '18.217.135.129'])
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ['gunicorn', ]
